5592901824f155715fbf806866fc4d358798ca12
zeno-46 <111848819+zeno-46@users.noreply.github.com>
2022-09-13 15:51:36 +0900

    ログインしていない状態だと特定のページへのアクセスが制限され、ログインページにリダイレクトすることを確認した

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!,only:[:new]
+  before_action :authenticate_user!,only:[:new, :edit]
   before_action :move_to_index, only: [:edit]
 
   def index
